ce38f1df77009e405c011e67062887365843b125
Laza <lazacuestalautaro@gmail.com>
2024-03-18 19:52:29 -0300

    deploy test

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const path = require("path");
+const Comments = require("./src/models/comments");
 const app = express();
 const port = 3000;
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/mongotest", (req, res) => {
+  const comments = Comments.find();
+
+  res.send(comments);
 });
 
 app.listen(port, () => {
---
+++ src/models/comments.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const comments = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Comments = model("Comments", comments);
+
+module.exports = { Comments };
